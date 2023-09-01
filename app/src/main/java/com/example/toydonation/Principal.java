dc1914ff08ab0f6307a1532914686ef753215cc8
Davi <werdavid1@gmail.com>
2023-09-01 12:16:48 -0300

    atualização

--- app/src/main/java/com/example/toydonation/Principal.java
+++ app/src/main/java/com/example/toydonation/Principal.java
@@ -2,13 +2,41 @@ package com.example.toydonation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class Principal extends AppCompatActivity {
+
+    ImageButton BtnBeneficio;
+    ImageButton BtnImportancia;
+    ImageButton BtnOndeDoar;
+    ImageButton BtnComoDoar;
+
+    // barra
+    ImageButton BtnPerfil;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+
+        // Instanciando os botões
+        BtnBeneficio = findViewById (R.id.BtnBeneficio);
+        BtnOndeDoar = findViewById (R.id.BtnOndeDoar);
+        BtnComoDoar = findViewById (R.id.BtnComoDoar);
+        BtnImportancia = findViewById (R.id.BtnImportancia);
+
+
+        BtnImportancia.setOnClickListener(new View.OnClickListener(){
+            @Override // verificar se tem no cod
+            public void onClick(View v){
+                Intent PqDoar = new Intent(Principal.this, PqDoar.class);
+                startActivity(PqDoar);
+            }
+        });
     }
 }
