45ee0f8eedcb62648e7ad56491b1ee3d02cc9157
lauracristinee <laura.silva390@etec.sp.gov.br>
2023-09-10 14:14:23 -0300

    Atualização Perfil

--- app/src/main/java/com/example/toydonation/Perfil.java
+++ app/src/main/java/com/example/toydonation/Perfil.java
@@ -2,13 +2,33 @@ package com.example.toydonation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+// Perfil ou MainActivity nessa parte?
 public class Perfil extends AppCompatActivity {
+    Button BtnDoar;
+    Button BtnBrinquedosDoados;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_perfil);
+        setContentView(R.layout.activity_perfil); // aqui a mesma coisa, é perfil ou main? main estava dando erro
+
+        BtnDoar = findViewById (R.id.BtnDoar);
+        BtnBrinquedosDoados = findViewById (R.id.BtnBrinquedosDoados);
+
+
+        BtnDoar.setOnClickListener(new View.OnClickListener(){
+            @Override // verificar se tem no cod
+            public void onClick(View d) {
+                Intent Doar = new Intent( Perfil.this, Ondedoar.class);
+                startActivity(Doar);
+
+                // Questão, podemos chamar a mesma intente ?
+            }
+        });
     }
 }
