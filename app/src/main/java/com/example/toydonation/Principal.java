5f877782e1201ef2c59929b471b5a79de79884cd
lauracristinee <laura.silva390@etec.sp.gov.br>
2023-09-10 13:53:52 -0300

    Acrescentado ao projeto, a Activity Perfil, seu layout e respectivo java

--- app/src/main/java/com/example/toydonation/Principal.java
+++ app/src/main/java/com/example/toydonation/Principal.java
@@ -38,5 +38,33 @@ public class Principal extends AppCompatActivity {
                 startActivity(Importancia);
             }
         });
+        BtnBeneficio.setOnClickListener(new View.OnClickListener() {
+            @Override // verificar se tem no cod
+            public void onClick(View b) {
+                Intent Beneficios = new Intent( Principal.this, Beneficio.class);
+                startActivity(Beneficios);
+            }
+        });
+        BtnOndeDoar.setOnClickListener(new View.OnClickListener() {
+            @Override // verificar se tem no cod
+            public void onClick(View o) {
+                Intent OndeDoar= new Intent( Principal.this, Ondedoar.class);
+                startActivity(OndeDoar);
+            }
+        });
+        BtnPerfil.setOnClickListener(new View.OnClickListener() {
+            @Override // verificar se tem no cod
+            public void onClick(View p) {
+                Intent Perifl= new Intent( Principal.this, Login.class); //tava perfil pq? "Perfil.class"
+                startActivity(Perifl);
+            }
+        });
+        BtnComoDoar.setOnClickListener(new View.OnClickListener() {
+            @Override // verificar se tem no cod
+            public void onClick(View c) {
+                Intent ComoDoar= new Intent( Principal.this, ComoDoar.class);
+                startActivity(ComoDoar);
+            }
+        });
     }
 }
