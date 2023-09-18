f494dac4df681b5964c1246bf08588c5967ad24f
lauracristinee <laura.silva390@etec.sp.gov.br>
2023-09-17 21:19:27 -0300

    projeto finalizado

--- app/src/main/java/com/example/toydonation/Login.java
+++ app/src/main/java/com/example/toydonation/Login.java
@@ -2,13 +2,26 @@ package com.example.toydonation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
-
+Button BtnLogin1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        BtnLogin1 = findViewById(R.id.BtnLogin1);
+
+        BtnLogin1.setOnClickListener(new View.OnClickListener(){
+            @Override // verificar se tem no cod
+            public void onClick(View usu){
+                Intent usuario = new Intent(Login.this, Perfil.class);
+                startActivity(usuario);
+            }
+        });
     }
 }
